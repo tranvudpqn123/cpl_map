52e74ed0025ee459f9ce4c76b353a32fc24e4408
quang1xc <2uang.anh.lee@gmail.com>
2025-01-14 16:56:01 +0700

    feat(SidebarComponent): create function get list service call api

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,28 +1,24 @@
 import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
-import {GoogleMapsModule} from '@angular/google-maps';
 // Services
 import { MerchantFilterService} from '@services/merchant-filter.service';
 // Components
 import {SidebarComponent} from '@pages/map/sidebar/sidebar.component';
-import {SliderPhotosComponent} from '@pages/map/slider-photos/slider-photos.component';
 import {FilterComponent} from '@pages/map/filter/filter.component';
-import {PersonalGroupsComponent} from '@pages/map/personal-groups/personal-groups.component';
-import {AddressDetailComponent} from '@pages/map/address-detail/address-detail.component';
 import {AutomaticallyUnsubscribe} from '@constants/automatically-unsubscribe';
 
 //Interface
 import {IMerchant} from '@models/merchant.interface';
+import {NgForOf} from '@angular/common';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
     selector: 'app-map',
     standalone: true,
     imports: [
         SidebarComponent,
-        SliderPhotosComponent,
         FilterComponent,
-        PersonalGroupsComponent,
-        AddressDetailComponent,
-        GoogleMapsModule
+        NgForOf,
+        ReactiveFormsModule,
     ],
     templateUrl: './map.component.html',
     styleUrl: './map.component.scss',
@@ -32,11 +28,13 @@ export class MapComponent extends AutomaticallyUnsubscribe implements OnInit{
     private readonly merchantFilterService = inject(MerchantFilterService);
     selectedMerchant = signal<IMerchant | null>(null);
     isShowMerchantGroups = signal<boolean>(false);
-    options: google.maps.MapOptions = {
-        mapId: "DEMO_MAP_ID",
-        center: { lat: -31, lng: 147 },
-        zoom: 4,
-    };
+    listServiceTypeMerchant = signal<any[]>([]);
+    // options: google.maps.MapOptions = {
+    //     mapId: "DEMO_MAP_ID",
+    //     center: { lat: -31, lng: 147 },
+    //     zoom: 4,
+    // };
+
 
     ngOnInit() {
         this.merchantFilterService.selectedMerchant$
@@ -47,5 +45,19 @@ export class MapComponent extends AutomaticallyUnsubscribe implements OnInit{
             .subscribe(isShowMerchantGroups => {
                 this.isShowMerchantGroups.set(isShowMerchantGroups);
             })
+        this.getListServiceType();
     }
+
+    getListServiceType()   {
+        this.merchantFilterService.getListServiceType().subscribe(res=>{
+            console.log(res)
+            const {data, code} = res;
+            if(code === '200'){
+                this.listServiceTypeMerchant.set(data);
+            }
+        })
+    }
+
+
+
 }
