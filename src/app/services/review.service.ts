d331249199bd60d812b7234a27cd5b349c5c349d
quang1xc <2uang.anh.lee@gmail.com>
2025-01-17 17:07:20 +0700

    update(address-review): create function get list review call api

---
+++ src/app/services/review.service.ts
@@ -0,0 +1,29 @@
+﻿import {inject, Injectable} from '@angular/core';
+import {map, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {IData, IResponseData} from '@models/response-data.interface';
+import {UtilsService} from '@services/utils.service';
+import {environment} from '@environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ReviewService {
+    private readonly httpClient = inject(HttpClient);
+    private readonly utilsService = inject(UtilsService);
+    private  readonly  API_URL = environment.apiUrl;
+
+    getRating(id: string | undefined): Observable<IResponseData<IData>> {
+        const data = {partner_id: id}
+        const url = `http://103.72.98.97/api/app/customer/home/listRating`;
+        return this.httpClient.post<IResponseData<IData>>(url,data,{
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjAzNzk2ODU5MzMiLCJuYW1laWQiOiJjdXN0b21lciIsImZhbWlseV9uYW1lIjoiMTg2MGUzYmQtMGVmMS00NWUwLWE5NzYtZDA2YzAyMmUzZjgyIiwibmJmIjoxNzM2OTEwMzAzLCJleHAiOjE3MzgxMTk5MDMsImlhdCI6MTczNjkxMDMwM30.G8Em2Rt7bsd5qg7wy_F7nl0WLB1cJ5O1KzVoqcMgZO0`
+            }
+        })
+            .pipe(map(res =>
+                this.utilsService.convertKeysToCamelCase<IResponseData<IData>>(res)));
+    }
+
+}
+
