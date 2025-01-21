8ac1d2766fd174b6720957f1ddba8c8c0582b3ec
quang1xc <2uang.anh.lee@gmail.com>
2025-01-21 17:48:26 +0700

    feat(ShowAllServiceType): create modal show list service-type and list sub service-type

--- src/app/pages/map/category/show-all-service-type/show-all-service-type.component.ts
+++ src/app/pages/map/category/show-all-service-type/show-all-service-type.component.ts
@@ -1,17 +1,17 @@
 import {
     ChangeDetectionStrategy,
     Component,
-    CUSTOM_ELEMENTS_SCHEMA,
-    inject, Input,
+    inject,
     OnInit, signal,
 } from '@angular/core';
-import {Swiper} from 'swiper';
 import {RedZoomModule} from 'ngx-red-zoom';
-import {IAddress, IAddressGroup, IAddressImageGroup} from '@models/address-merchant.interface';
-import {IServiceType, MerchantFilterService} from '@services/merchant-filter.service';
+import {IServiceType, ISubServiceType, MerchantFilterService} from '@services/merchant-filter.service';
 import {CommonModule} from '@angular/common';
 import {CategoryService} from '@services/category.service';
-import {ICategory} from '@models/category.interface';
+import {forkJoin} from "rxjs";
+import {Dialog} from "@angular/cdk/dialog";
+import {IconPaths} from "@constants/image-paths";
+import {SafeSvgPipe} from "@pipes/safe-svg.pipe";
 
 @Component({
     selector: 'app-show-service-type',
@@ -19,23 +19,42 @@ import {ICategory} from '@models/category.interface';
     templateUrl: './show-all-service-type.component.html',
     styleUrl: './show-all-service-type.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RedZoomModule, CommonModule],
+    imports: [RedZoomModule, CommonModule, SafeSvgPipe],
 })
 export class ShowAllServiceTypeComponent implements OnInit {
     private readonly categoryService = inject(CategoryService);
-    listServiceTypes = signal<IServiceType[]>([])
-    listSubServiceTypes = signal<IServiceType[]>([])
-    ngOnInit() {
-        this.categoryService.listServiceType.subscribe(data=>{
-            console.log(data);
-            this.listServiceTypes.set(data);
-        })
+    private readonly merchantFilterService = inject(MerchantFilterService);
+    private readonly dialog = inject(Dialog);
 
-        this.categoryService.listServiceType.subscribe(data=>{
-            console.log(data);
+    listServiceTypes = signal<IServiceType[]>([])
+    listSubServiceTypeCache = signal<Record<string, any[]>>({});
+
+    ngOnInit() {
+        this.categoryService.listServiceType.subscribe(data => {
             this.listServiceTypes.set(data);
-        })
+
+            const requests = data.map(itemServiceType =>
+                this.categoryService.getListSubServiceType(itemServiceType.id)
+            );
+            forkJoin(requests).subscribe(results => {
+                const cache: Record<string, any[]> = {};
+                results.forEach((res, index) => {
+                    cache[data[index].id] = res.data;
+                });
+                this.listSubServiceTypeCache.set(cache);
+            });
+        });
+    }
+
+    onSelectSubServiceType(subService: ISubServiceType) {
+        this.merchantFilterService.updateSelectedSubService(subService);
+        this.closeModal();
+    }
+
+    closeModal(){
+        this.dialog.closeAll();
     }
 
 
+    protected readonly IconPaths = IconPaths;
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -13,7 +13,7 @@ export class CategoryService {
     private readonly httpClient = inject(HttpClient);
     private readonly utilsService = inject(UtilsService);
     readonly listServiceType = new BehaviorSubject<IServiceType[]>([]);
-    private readonly listSubServiceType = new BehaviorSubject<IServiceType[]>([]);
+    readonly listSubServiceType = new BehaviorSubject<any[]>([]);
 
     private  readonly  API_URL = environment.apiUrl;
     // getListServiceType()   {
@@ -27,16 +27,16 @@ export class CategoryService {
     //             this.utilsService.convertKeysToCamelCase<IResponseData<any>>(res)));
     // }
     //
-    // getListSubServiceType(id: string)   {
-    //     const url = `http://103.72.98.97/api/app/dropdownapp/subservicetype` + `?service_type_id=${id}`;
-    //     return this.httpClient.get<IResponseData<string>>(url,{
-    //         headers: {
-    //             Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjAzNzk2ODU5MzMiLCJuYW1laWQiOiJjdXN0b21lciIsImZhbWlseV9uYW1lIjoiMTg2MGUzYmQtMGVmMS00NWUwLWE5NzYtZDA2YzAyMmUzZjgyIiwibmJmIjoxNzM2OTEwMzAzLCJleHAiOjE3MzgxMTk5MDMsImlhdCI6MTczNjkxMDMwM30.G8Em2Rt7bsd5qg7wy_F7nl0WLB1cJ5O1KzVoqcMgZO0`
-    //         }
-    //     })
-    //         .pipe(map(res =>
-    //             this.utilsService.convertKeysToCamelCase<IResponseData<any>>(res)));
-    // }
+    getListSubServiceType(id: string)   {
+        const url = `http://103.72.98.97/api/app/dropdownapp/subservicetype` + `?service_type_id=${id}`;
+        return this.httpClient.get<IResponseData<string>>(url,{
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjAzNzk2ODU5MzMiLCJuYW1laWQiOiJjdXN0b21lciIsImZhbWlseV9uYW1lIjoiMTg2MGUzYmQtMGVmMS00NWUwLWE5NzYtZDA2YzAyMmUzZjgyIiwibmJmIjoxNzM2OTEwMzAzLCJleHAiOjE3MzgxMTk5MDMsImlhdCI6MTczNjkxMDMwM30.G8Em2Rt7bsd5qg7wy_F7nl0WLB1cJ5O1KzVoqcMgZO0`
+            }
+        })
+            .pipe(map(res =>
+                this.utilsService.convertKeysToCamelCase<IResponseData<any>>(res)));
+    }
 
     addListServiceType(data: IServiceType[]){
         this.listServiceType.next(data);
