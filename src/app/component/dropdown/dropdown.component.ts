ae820903141dda72d72bbf40f195bfc70c1a0e12
quang1xc <2uang.anh.lee@gmail.com>
2025-01-23 11:28:57 +0700

    create component dropdown

---
+++ src/app/component/dropdown/dropdown.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.scss'
+})
+export class DropdownComponent {
+
+    @Input() data: any;
+}
